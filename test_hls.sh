d0151aff2867b18d646c45f5df23bb2fadb35703
le blond erwan <erwanleblond@gmail.com>
2025-11-09 04:44:52 +0100

    fix cmd line

--- test_hls.sh
+++ test_hls.sh
@@ -4,6 +4,6 @@ if [ -z "${1}" ]; then
 fi
 
 packager \
-  "in=${1},stream=video,playlist_name=video.m3u8,segment_template=seg_\$Number\$.m4s" \
+  "in=${1},stream=video,playlist_name=video.m3u8,segment_template=seg_\$Number$.ts" \
   --hls_master_playlist_output /home/master.m3u8 \
   --v 3
