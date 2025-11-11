47650182742c95881f9c9d8e68da739ade66e6d7
le blond erwan <erwanleblond@gmail.com>
2025-11-11 17:57:49 +0100

    back up quick fix

--- packager/media/codecs/h266_parser.h
+++ packager/media/codecs/h266_parser.h
@@ -681,17 +681,6 @@ struct H266RepFormat {
   int conf_win_vps_bottom_offset = 0;
 };
 
-struct H266ProfileTierLevel {
-  uint8_t general_profile_idc;
-  uint8_t general_tier_flag;
-  uint8_t general_level_idc;
-  bool general_frame_only_constraint_flag;
-  bool general_non_packed_constraint_flag;
-  bool general_interlaced_source_flag;
-  bool general_progressive_source_flag;
-  // ... autres champs du profile tier level
-};
-
 
 struct H266Vps {
   H266Vps();
