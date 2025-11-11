823970b147146576fb5f8c88925d7e28eb7d7a21
le blond erwan <erwanleblond@gmail.com>
2025-11-11 10:32:25 +0100

    add first general contraint info

--- packager/media/codecs/h266_parser.cc
+++ packager/media/codecs/h266_parser.cc
@@ -1446,6 +1446,97 @@ H266Parser::Result H266Parser::ParseProfileTierLevel(bool profile_tier_present,
   return kOk;
 }
 
+H266Parser::Result H266Parser::ParseGeneralConstraintsInfo(H266GeneralConstraintsInfo *gci,
+                                                     H26xBitReader* br) {
+TRUE_OR_RETURN(br->ReadBool(&gci->gci_present_flag);
+/* general */
+if (gci->gci_present_flag){
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_intra_only_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_all_layers_independent_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_one_au_only_constraint_flag));
+    /* picture format */
+    TRUE_OR_RETURN(br->ReadBool(4,&gci->gci_sixteen_minus_max_bitdepth_constraint_idc)); //4 bits
+    TRUE_OR_RETURN(br->ReadBool(2,&gci->gci_three_minus_max_chroma_format_constraint_idc));//2 bits
+    /* NAL unit type related */
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_mixed_nalu_types_in_pic_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_trail_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_stsa_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_rasl_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_radl_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_idr_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_cra_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_gdr_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_aps_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_idr_rpl_constraint_flag));
+    /* tile, slice, subpicture partitioning */
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_one_tile_per_pic_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_pic_header_in_slice_header_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_one_slice_per_pic_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_rectangular_slice_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_one_slice_per_subpic_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_subpic_info_constraint_flag));
+    /* CTU and block partitioning */
+    int gci_three_minus_max_log2_ctu_size_constraint_idc)); //2 bits
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_partition_constraints_override_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_mtt_constraint_flag
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_qtbtt_dual_tree_intra_constraint_flag));
+    /* intra */
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_palette_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ibc_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_isp_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_mrl_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_mip_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_cclm_constraint_flag));
+    /* inter */
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ref_pic_resampling_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_res_change_in_clvs_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_weighted_prediction_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ref_wraparound_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_temporal_mvp_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_amvr_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_bdof_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_smvd_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_dmvr_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_mmvd_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_affine_motion_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_prof_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_bcw_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ciip_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_gpm_constraint_flag));
+    /* transform, quantization, residual */
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_luma_transform_size_64_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_transform_skip_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_bdpcm_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_mts_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_lfnst_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_joint_cbcr_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_sbt_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_act_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_explicit_scaling_list_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_dep_quant_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_sign_data_hiding_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_cu_qp_delta_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_chroma_qp_offset_constraint_flag));
+    /* loop filter */
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_sao_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_alf_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ccalf_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_lmcs_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ladf_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_virtual_boundaries_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(8,&gci->gci_num_additional_bits)); //8 bits
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_all_rap_pictures_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_extended_precision_processing_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_ts_residual_coding_rice_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_rrc_rice_extension_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_persistent_rice_adaptation_constraint_flag));
+    TRUE_OR_RETURN(br->ReadBool(&gci->gci_no_reverse_last_sig_coeff_constraint_flag));
+}
+    return kOk;
+
+}
+
+
 H266Parser::Result H266Parser::SkipScalingListData(H26xBitReader* br) {
   // H.266 scaling list data parsing would go here
   // Similar to H.265 but with potential differences
--- packager/media/codecs/h266_parser.h
+++ packager/media/codecs/h266_parser.h
@@ -118,6 +118,111 @@ struct H266VuiParameters {
   u_int vui_chroma_sample_loc_type_bottom_field;
   // Incomplete...
 };
+struct H266GeneralConstraintsInfo{
+bool gci_present_flag
+/* general */
+bool gci_intra_only_constraint_flag;
+bool gci_all_layers_independent_constraint_flag;
+bool gci_one_au_only_constraint_flag;
+/* picture format */
+int gci_sixteen_minus_max_bitdepth_constraint_idc; //4 bits
+int gci_three_minus_max_chroma_format_constraint_idc;//2 bits
+/* NAL unit type related */
+bool gci_no_mixed_nalu_types_in_pic_constraint_flag;
+bool gci_no_trail_constraint_flag;
+bool gci_no_stsa_constraint_flag;
+bool gci_no_rasl_constraint_flag;
+bool gci_no_radl_constraint_flag;
+bool gci_no_idr_constraint_flag;
+bool gci_no_cra_constraint_flag;
+bool gci_no_gdr_constraint_flag;
+bool gci_no_aps_constraint_flag;
+bool gci_no_idr_rpl_constraint_flag;
+/* tile, slice, subpicture partitioning */
+bool gci_one_tile_per_pic_constraint_flag;
+bool gci_pic_header_in_slice_header_constraint_flag;
+bool gci_one_slice_per_pic_constraint_flag;
+bool gci_no_rectangular_slice_constraint_flag;
+bool gci_one_slice_per_subpic_constraint_flag;
+bool gci_no_subpic_info_constraint_flag;
+/* CTU and block partitioning */
+int gci_three_minus_max_log2_ctu_size_constraint_idc; //2 bits
+bool gci_no_partition_constraints_override_constraint_flag;
+bool gci_no_mtt_constraint_flag
+bool gci_no_qtbtt_dual_tree_intra_constraint_flag;
+/* intra */
+bool gci_no_palette_constraint_flag;
+bool gci_no_ibc_constraint_flag;
+bool gci_no_isp_constraint_flag;
+bool gci_no_mrl_constraint_flag;
+bool gci_no_mip_constraint_flag;
+bool gci_no_cclm_constraint_flag;
+/* inter */
+bool gci_no_ref_pic_resampling_constraint_flag;
+bool gci_no_res_change_in_clvs_constraint_flag;
+bool gci_no_weighted_prediction_constraint_flag;
+bool gci_no_ref_wraparound_constraint_flag;
+bool gci_no_temporal_mvp_constraint_flag;
+bool gci_no_amvr_constraint_flag;
+bool gci_no_bdof_constraint_flag;
+bool gci_no_smvd_constraint_flag;
+bool gci_no_dmvr_constraint_flag;
+bool gci_no_mmvd_constraint_flag;
+bool gci_no_affine_motion_constraint_flag;
+bool gci_no_prof_constraint_flag;
+bool gci_no_bcw_constraint_flag;
+bool gci_no_ciip_constraint_flag;
+bool gci_no_gpm_constraint_flag;
+/* transform, quantization, residual */
+bool gci_no_luma_transform_size_64_constraint_flag;
+bool gci_no_transform_skip_constraint_flag;
+bool gci_no_bdpcm_constraint_flag;
+bool gci_no_mts_constraint_flag;
+bool gci_no_lfnst_constraint_flag;
+bool gci_no_joint_cbcr_constraint_flag;
+bool gci_no_sbt_constraint_flag;
+bool gci_no_act_constraint_flag;
+bool gci_no_explicit_scaling_list_constraint_flag;
+bool gci_no_dep_quant_constraint_flag;
+bool gci_no_sign_data_hiding_constraint_flag;
+bool gci_no_cu_qp_delta_constraint_flag;
+bool gci_no_chroma_qp_offset_constraint_flag;
+/* loop filter */
+bool gci_no_sao_constraint_flag;
+bool gci_no_alf_constraint_flag;
+bool gci_no_ccalf_constraint_flag;
+bool gci_no_lmcs_constraint_flag;
+bool gci_no_ladf_constraint_flag;
+bool gci_no_virtual_boundaries_constraint_flag;
+int gci_num_additional_bits; //8 bits
+bool gci_all_rap_pictures_constraint_flag;
+bool gci_no_extended_precision_processing_constraint_flag;
+bool gci_no_ts_residual_coding_rice_constraint_flag;
+bool gci_no_rrc_rice_extension_constraint_flag;
+bool gci_no_persistent_rice_adaptation_constraint_flag;
+bool gci_no_reverse_last_sig_coeff_constraint_flag;
+
+};
+
+struct H266ProfileTierLevel{
+  int general_profile_idc;
+  bool general_tier_flag;
+  int general_level_idc;
+  bool ptl_frame_only_constraint_flag;
+  bool ptl_multilayer_enabled_flag;
+
+  H266GeneralConstraintsInfo gci;
+
+  std::vector <bool> ptl_sublayer_level_present_flag;
+  std::vector <int> sublayer_level_idc;
+  std::vector <int> sublayer_level_idc;
+  int ptl_num_sub_profiles;
+  std::vector <u_int32_t> general_sub_profile_idc;
+
+
+};
+
+
 
 struct H266Pps {
   H266Pps();
@@ -928,6 +1033,8 @@ class H266Parser {
                             const H266Sps& sps,
                             H26xBitReader* br,
                             H266ReferencePicListStruct* rpls);
+  Result H266Parser::ParseGeneralConstraintsInfo(H266GeneralConstraintsInfo *gci,
+                                                     H26xBitReader* br);
 
   Result H266Parser::GetGeneralTimingHrdParameters(GeneralTimingHrdParameters *time,
                                       H26xBitReader* br);                          
