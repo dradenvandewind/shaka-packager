9474688d66009647678768aeda730224f1e0baa9
le blond erwan <erwanleblond@gmail.com>
2025-12-04 14:29:12 +0100

    fix

--- packager/media/codecs/h266_parser.cc
+++ packager/media/codecs/h266_parser.cc
@@ -185,7 +185,7 @@ H266Sps::~H266Sps() {}
 H266Vps::H266Vps() {}
 H266Vps::~H266Vps() {}
 
-H266Aps::H266Aps() {}
+//H266Aps::H266Aps() {}
 H266Aps::~H266Aps() {}
 
 H266PictureHeader::H266PictureHeader() {}
